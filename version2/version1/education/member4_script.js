df727fecd6d8db7c35f6181acddebb26ba13f7cd
EugeneTee6378 <0714eugenetee@gmail.com>
2026-08-08 22:50:41 +0800

    debug and fix the code.

--- version2/version1/education/member4_script.js
+++
@@ -1,6 +0,0 @@
-var downloadLink = document.getElementById("download-link");
-var message = document.getElementById("download-message");
-
-downloadLink.onclick = function () {
-  message.innerHTML = "Preparing Saif_Pathan_CV.pdf for download\u2026 (simulated download started)";
-};
